7750726c51d7135893a6c1d6ec4d2af63aa57bb6
Kristian Hanekamp <kris.hanekamp@gmail.com>
2011-09-22 19:35:13 +0200

    wrote some rdoc
    and gitignored build files

--- lib/helpful_configuration.rb
+++ lib/helpful_configuration.rb
@@ -1,15 +1,30 @@
+# Example:
+#
+# during initialization of your app:
+#
+# my_configuration_file = Pathname("config.json")
+# config = HelpfulConfiguration.new(JSON.parse(my_configuration_file.read), my_configuration_file)
+# config.explain("spam_destination", "where should the application send it's spam?")
+#
+# later in your app:
+# my_spam.send_to(config["spam_destination"])
+#
 class HelpfulConfiguration
   attr_accessor :file
 
   class ConfigurationMissingError < StandardError
   end
 
+  # provide the configuration and the file name where it is stored.
   def initialize(configuration = {}, file=nil)
     @configuration = configuration
     @explanation = {}
     self.file = file
   end
 
+  # access configuration keys.
+  # raises an error when the key has not been explained.
+  # raises ConfigurationMissingError when the key has not been set.
   def [](key)
     key = key.to_s
     assert_explanation(key)
@@ -23,21 +38,25 @@ class HelpfulConfiguration
     end
   end
 
+  # set configuration keys.
   def []=(key, value)
     key = key.to_s
     @configuration[key] = value
   end
 
+  # provide explanations for configuration keys.
   def explain(key, explanation)
     key = key.to_s
     @explanation[key] = explanation
   end
 
+  # check if a certain key has been configured by the user.
   def configured?(key)
     assert_explanation(key)
     @configuration.has_key?(key)
   end
 
+  # return the value of the configuration key or if it is missing, the given default.
   def with_default(default, key)
     assert_explanation(key)
     configured?(key) ? self[key] : default
